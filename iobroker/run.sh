2981b21cca36460d4714214a6338226043d70cde
alientim <tim@eertmoed.de>
2025-03-03 13:41:32 +0100

    Update run.sh

--- iobroker/run.sh
+++ iobroker/run.sh
@@ -1,8 +1,13 @@
-#!/usr/bin/with-contenv sh
-if [ ! -S /var/run/docker.sock ]; then
-  echo "Docker-Socket nicht gefunden!"
-  exit 1
-fi
+#!/bin/bash
 
-echo "Starte ioBroker-Container..."
-docker run --rm --name iobroker -p 8081:8081 buanet/iobroker:latest
+# Setze die Konfigurationen aus der config.json
+IOBROKER_PORT=${IOBROKER_PORT:-8081}  # Standardwert 8081, falls nicht gesetzt
+MQTT_PORT=${MQTT_PORT:-1883}  # Standardwert 18883, falls nicht gesetzt
+
+# Starte den Docker-Container mit den konfigurierten Ports
+docker run -d \
+  -p ${IOBROKER_PORT}:8081 \
+  -p ${MQTT_PORT}:1883 \
+  --name iobroker \
+  --restart unless-stopped \
+  buanet/iobroker:latest
