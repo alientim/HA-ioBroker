b595fcbdd87cf80de72c8620ecf45001a49133a1
alientim <tim@eertmoed.de>
2025-03-04 12:03:12 +0100

    Update run.sh

--- iobroker/run.sh
+++ iobroker/run.sh
@@ -5,10 +5,10 @@ PORT_ADMIN=$(jq --raw-output '.port_admin' $CONFIG_PATH)
 PORT_MQTT=$(jq --raw-output '.port_mqtt' $CONFIG_PATH)
 
 # Erstellen Sie das Verzeichnis für persistente Daten, falls es nicht existiert
-mkdir -p /config/addons_config/iobroker
+mkdir -p /addon_configs/iobroker
 
 /usr/bin/docker run -d \
-  -v /config/addons_config/iobroker:/opt/iobroker \
+  -v /addon_configs/iobroker:/opt/iobroker \
   -p $PORT_ADMIN:8081 \
   -p $PORT_MQTT:1883 \
   buanet/iobroker
